e4af37bb9dd7b93b11b18118d57938aa87c3b74d
Nick Collins <n0488629@actx.edu>
2025-03-23 09:10:30 -0500

    initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;  // Import the Scanner class
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in); // create scanner object
+      System.out.println("Interest Calculator Program");
+      System.out.println("What is the principal amount of the laon in dollars?");
+      int loan = myObj.nextInt(); // read user input
+      System.out.println("What is the interest rate (input 0.05 for 5%)?");
+      double interest = myObj.nextDouble(); // read user input
+      System.out.println("What is period of loan in years?");
+      int years = myObj.nextInt(); // read user input
+
+      double total = loan * interest * years; // calculate total
+      System.out.println("Total interest of Loan: " + total + " dollars."); // output total to user
+
+      myObj.close(); // close scanner object
+    }
+  }
